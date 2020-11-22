306a24d85c559c067d9999fe0e760b432ad9cd66
SonTrungTo <son.trung.to@gmail.com>
2020-11-22 06:19:07 +0200

    basic api-user written...
    We are now writing a slightly more
    advanced user data for submitting photos.
    First, we config new route for reqesting photo,
    then fetching photo with a new api-user method.

---
+++ client/user/Signup.js
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+import Icon from "@material-ui/core/Icon";
+import Error from "@material-ui/icons/Error";
+import { makeStyles } from "@material-ui/core/styles";
