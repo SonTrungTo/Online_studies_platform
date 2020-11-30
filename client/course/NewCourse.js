d9f85a4a056575fa9fe0fa6f2487c366f2214882
SonTrungTo <son.trung.to@gmail.com>
2020-11-30 16:54:40 +0200

    client API written. client components writing...

---
+++ client/course/NewCourse.js
@@ -0,0 +1,8 @@
+import React, { useState } from "react";
+import { create } from "./api-course";
+import auth from "../auth/auth-helper";
+import Card from "@material-ui/core/Card";
+
+export default function NewCourse() {
+    
+};
---
+++ client/course/api-course.js
@@ -0,0 +1,19 @@
+const create = async (params, credentials, course) => {
+    try {
+        const response = await fetch('/api/courses/by/' + params.userId, {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Authorization': 'Bearer ' + credentials.t
+            },
+            body: course
+        });
+        return await response.json();
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+export default {
+    create
+};
